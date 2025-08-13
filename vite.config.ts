62ae066a2d01b759ff32085f58d87fc8a02e6eec
Sergi Florensa <sergiflorensadj@gmail.com>
2025-08-13 11:04:08 +0200

    front

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/restaurantepablo/', // 👈 exacto al nombre del repo
+  base: '/restaurantePablo/', // 👈 MUY IMPORTANTE para GitHub Pages
 })
